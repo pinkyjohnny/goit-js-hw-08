c836f7a94fbba6898636c451e2a2cddb516285c4
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-21 21:09:36 +0300

    tasks-update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle'
 
+const refs = {
+    formElem: document.querySelector(".feedback-form")
+}
+
+refs.formElem.addEventListener('input', onFormInput)
+
+const obj = {};
+
+function onFormInput(ev) {
+    const value = ev.target.value;
+    const key = ev.target.name;
+    obj[key] = value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj))
+}
+
+function onLoadPage() {
+    const data = localStorage.getItem('feedback-form-state')
+    const parseData = JSON.parse(data)
+    refs.formElem.elements.email.value = parseData.email;
+    refs.formElem.elements.message.value = parseData.message;
+}
+onLoadPage()
+
+document.querySelector('form').addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    var email = document.querySelector('input[name="email"]').value;
+    var message = document.querySelector('textarea[name="message"]').value;
+
+    console.log({ email: email, message: message });
+
+    document.querySelector('input[name="email"]').value = '';
+    document.querySelector('textarea[name="message"]').value = '';
+};
+
+const updateStorage = throttle(() => {
+    localStorage.clear();
+}, 500);
+updateStorage();
